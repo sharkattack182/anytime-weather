97f6074ddc88add803ebddc83ed67512b782d97b
Matthew Courtney <matthewdc182@gmail.com>
2021-06-11 00:01:08 -0400

    one last commit before bed

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <template>
   <div id="app">
-
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search..." />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-}
+  name: "App",
+  data() {
+    return {
+      api_key: "60bc1e306b6fedb9805794cb8ab47ed3",
+    };
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+} 
+
+body {
+  font-family: "montserrat", sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url("./assets/warm.png");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75));
 }
 </style>
